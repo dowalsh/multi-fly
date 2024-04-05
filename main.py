5c015349103bb1148d9c54ea761f9ad0ea9dc0ee
Dylan Walsh <dylanwitsend@gmail.com>
2024-04-05 18:28:36 -0400

    API key read in

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # hello world
 def main():
     print("Hello World")
+    # read in api key from file
+    with open('api_key.txt', 'r') as file:
+        api_key = file.read().replace('\n', '')
+    
+    print(api_key)
 
 if __name__ == '__main__':
     main()
